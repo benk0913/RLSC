7e33c4c6040dd20f4e6d62c47cd67aad72882be5
benk0913 <benk0913@gmail.com>
2020-12-06 11:10:34 +0200

    crappy earth rock spell wip

---
+++ Assets/Scripts/Sprite/RandomSpriteEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpriteEntity : MonoBehaviour
+{
+    [SerializeField]
+    Sprite[] Varriety;
+
+    [SerializeField]
+    SpriteRenderer Renderer;
+
+    [SerializeField]
+    float targetHeight = 1f;
+
+    private void OnEnable()
+    {
+        Renderer.sprite = Varriety[Random.Range(0, Varriety.Length)];
+
+        Bounds bounds = Renderer.sprite.bounds;
+        float factor = targetHeight / bounds.size.y;
+        transform.localScale = new Vector3(factor, factor, factor);
+    }
+}
