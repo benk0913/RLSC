07b3be5cd2144ef45b8a3d44f3db5afe17d8f0c0
benk0913 <benk0913@gmail.com>
2021-01-28 18:30:11 +0200

    nombres

---
+++ Assets/Scripts/UI/NamePanelUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NamePanelUI : MonoBehaviour
+{
+    [SerializeField]
+    Actor CurrentActor;
+
+    [SerializeField]
+    TextMeshProUGUI TextLabel;
+    
+
+    void Start()
+    {
+        TextLabel.text = CurrentActor.State.Data.name;
+    }
+}
