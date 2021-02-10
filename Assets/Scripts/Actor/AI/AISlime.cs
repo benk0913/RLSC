515b455a09345e1c75667cdd52aa16f77a9fc1d7
benk0913 <benk0913@gmail.com>
2021-02-10 13:51:16 +0200

    More slime

---
+++ Assets/Scripts/Actor/AI/AISlime.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISlime : ActorAI
+{
+
+    protected override IEnumerator AIBehaviourRoutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        while (true)
+        {
+            AbilityState SelectedAbility = null;
+
+            while (SelectedAbility == null)
+            {
+                if (Act.State.Data.hp < (Act.State.Data.MaxHP*0.2f))
+                {
+                    SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Slime Split" && x.CurrentCD <= 0f);
+
+                    if (SelectedAbility == null)
+                    {
+                        SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Slime Hop" && x.CurrentCD <= 0f);
+                    }
+                }
+                else
+                {
+                    SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Slime Hop" && x.CurrentCD <= 0f);
+                }
+
+                WaitBehaviour();
+
+                yield return 0;
+            }
+
+            yield return StartCoroutine(UseAbilityRoutine(SelectedAbility));
+
+            yield return 0;
+        }
+    }
+
+}
