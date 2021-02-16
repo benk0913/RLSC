a18dc320909614960cbbf6583df48aadb3e63891
Tzook <tzook10@gmail.com>
2021-02-16 15:40:17 +0200

    Make snek look towards hit direction

--- Assets/Scripts/Actor/AI/AISnake.cs
+++ Assets/Scripts/Actor/AI/AISnake.cs
@@ -21,22 +21,6 @@ public class AISnake : ActorAI
         }
     }
 
-    public bool HasEnemiesToRight
-    {
-        get
-        {
-            foreach (ActorData actor in CORE.Instance.Room.Actors)
-            {
-                if (actor.ActorEntity != null && actor.isCharacter && actor.ActorEntity.transform.position.x > transform.position.x)
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-    }
-
     protected override IEnumerator AIBehaviourRoutine()
     {
         yield return new WaitForSeconds(0.5f);
@@ -50,52 +34,19 @@ public class AISnake : ActorAI
 
                 if (Random.Range(0, 4) == 0)
                 {
-                    if (!HasEnemiesToLeft)
-                    {
-                        SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Snake Hypno 2" && x.CurrentCD <= 0f);
-                    }
-                    else if (!HasEnemiesToRight)
-                    {
-                        SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Snake Hypno 1" && x.CurrentCD <= 0f);
-                    }
-                    else
-                    {
-                        int rndDir = Random.Range(1, 3);
-                        SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Snake Hypno " + rndDir && x.CurrentCD <= 0f);
-                    }
+                    SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Snake Hypno 1" && x.CurrentCD <= 0f);
                 }
                 else
                 {
-                    if (!HasEnemiesToLeft)
+                    if (Random.Range(0, 2) == 0)
                     {
-                        if (Random.Range(0, 2) == 0)
-                        {
-                            SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Snake Bite 2" && x.CurrentCD <= 0f);
-                        }
-                        else
-                        {
-                            SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Snake Bite 4" && x.CurrentCD <= 0f);
-                        }
-                    }
-                    else if (!HasEnemiesToRight)
-                    {
-                        if (Random.Range(0, 2) == 0)
-                        {
-                            SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Snake Bite 1" && x.CurrentCD <= 0f);
-                        }
-                        else
-                        {
-                            SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Snake Bite 3" && x.CurrentCD <= 0f);
-                        }
+                        SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Snake Bite 1" && x.CurrentCD <= 0f);
                     }
                     else
                     {
-
-                        int rndDir = Random.Range(1, 5);
-                        SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Snake Bite " + rndDir && x.CurrentCD <= 0f);
+                        SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Snake Bite 3" && x.CurrentCD <= 0f);
                     }
                 }
-               
 
                 WaitBehaviour();
 
@@ -110,6 +61,13 @@ public class AISnake : ActorAI
 
     protected override void MoveToTarget()
     {
-        return;
+        if (!HasEnemiesToLeft)
+        {
+            Act.AttemptLookRight();
+        }
+        else
+        {
+            Act.AttemptLookLeft();
+        }
     }
 }
