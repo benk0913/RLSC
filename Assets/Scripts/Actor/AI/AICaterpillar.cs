5dcf36deaf7108b3929fede349021ba82b17c756
benk0913 <benk0913@gmail.com>
2021-02-24 15:21:26 +0200

    Caterpillar

---
+++ Assets/Scripts/Actor/AI/AICaterpillar.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICaterpillar : ActorAI
+{
+    protected override void Start()
+    {
+        if (CurrentTarget.transform.position.x > transform.position.x)
+        {
+            Act.AttemptLookRight();
+        }
+        else
+        {
+            Act.AttemptLookLeft();
+        }
+
+        base.Start();
+    }
+
+    protected override IEnumerator AIBehaviourRoutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        while (true)
+        {
+       
+            AbilityState SelectedAbility = null;
+
+            while (SelectedAbility == null)
+            {
+
+                if (Act.State.Data.hp < Act.State.Data.MaxHP / 3f)
+                {
+                    SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "CaterRejuv" && x.CurrentCD <= 0f);
+                }
+                else
+                {
+                    SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "CaterBlow" && x.CurrentCD <= 0f);
+                }
+
+                WaitBehaviour();
+
+                yield return 0;
+            }
+
+            yield return 0;
+
+            yield return StartCoroutine(UseAbilityRoutine(SelectedAbility));
+        }
+    }
+
+}
