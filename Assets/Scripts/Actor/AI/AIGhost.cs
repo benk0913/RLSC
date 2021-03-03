7db261c381d2379230829852b195c0d4116fe61c
benk0913 <benk0913@gmail.com>
2021-03-03 15:58:44 +0200

    Ghost Improvemenmts

---
+++ Assets/Scripts/Actor/AI/AIGhost.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIGhost : ActorAI
+{
+    public GameObject EnragedEffect;
+
+
+
+    protected override IEnumerator AIBehaviourRoutine()
+    {
+        yield return 0;
+        while (true)
+        {
+            AbilityState SelectedAbility = null;
+
+            while (SelectedAbility == null)
+            {
+                if (Act.State.Data.hp < Act.State.Data.MaxHP /2f)
+                {
+                    SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Ghost Possess" && x.CurrentCD <= 0f);
+                    
+                    if(SelectedAbility == null)
+                    {
+                        SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Ghostial Reach" && x.CurrentCD <= 0f);
+                    }
+
+                    if (SelectedAbility == null)
+                    {
+                        SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Ghostial Pull" && x.CurrentCD <= 0f);
+                    }
+
+                    if (!EnragedEffect.activeInHierarchy)
+                    {
+                        EnragedEffect.SetActive(true);
+                    }
+                }
+                else
+                {
+                    SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Ghostial Possess" && x.CurrentCD <= 0f);
+
+                    if (SelectedAbility == null)
+                    {
+                        SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Ghostial Pull" && x.CurrentCD <= 0f);
+                    }
+
+                    if (EnragedEffect.activeInHierarchy)
+                    {
+                        EnragedEffect.SetActive(false);
+                    }
+                }
+
+                WaitBehaviour();
+
+                yield return 0;
+            }
+            
+            yield return new WaitForSeconds(Random.Range(0f,0.2f));
+
+            yield return StartCoroutine(UseAbilityRoutine(SelectedAbility));
+            
+
+            yield return 0;
+        }
+    }
+
+
+    
+
+}
