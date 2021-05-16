25d7b000ac8e4c19ee2661da2298417995f4f12d
benk0913 <benk0913@gmail.com>
2021-05-16 16:57:37 +0300

    Stuff

--- Assets/Scripts/Actor/Actor.cs
+++ Assets/Scripts/Actor/Actor.cs
@@ -35,7 +35,7 @@ public class Actor : MonoBehaviour
     protected SpriteRenderer Shadow;
 
     [SerializeField]
-    SpriteColorGroup spriteColorGroup;
+    public SpriteColorGroup spriteColorGroup;
 
     [SerializeField]
     public Collider2D Collider;
@@ -953,6 +953,16 @@ public class Actor : MonoBehaviour
         }
 
         Skin.RefreshLooks();
+    }
+
+    public void RefreshOrbs()
+    {
+        if (Skin == null)
+        {
+            return;
+        }
+
+        Skin.RefreshOrbs();
     }
 
     public void PutAbilitiesOnCooldown()
---
+++ Assets/Scripts/Entity/OrbCollider.cs
@@ -0,0 +1,63 @@
+﻿using SimpleJSON;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OrbCollider : HitCollider
+{
+    public const float SIZE_STANDARD = 2f;
+
+    public Item OrbSource;
+
+    public bool StickToActor;
+
+    public bool HitCollider;
+
+    [SerializeField]
+    Animator Anim;
+
+    private void Awake()
+    {
+        if(Anim ==null)
+        {
+            Anim = GetComponent<Animator>();
+        }
+    }
+
+    private void OnEnable()
+    {
+        CORE.Instance.DelayedInvokation(0.1f, () =>
+        {
+            float SizeValue = (ActorSource.Collider.bounds.extents.magnitude / SIZE_STANDARD);
+
+            transform.position += transform.TransformDirection(0f, SizeValue, 0f);
+
+            transform.localScale = Vector3.one * SizeValue;
+        });
+    }
+
+
+    protected void Update()
+    {
+        if(StickToActor)
+        {
+            if(ActorSource == null)
+            {
+                this.gameObject.SetActive(false);
+                return;
+            }
+            transform.position = ActorSource.transform.position;
+        }
+
+    }
+
+    public void SetInfo(Item orbSource, Actor actorSource)
+    {
+        OrbSource = orbSource;
+        ActorSource = actorSource;
+    }
+    
+
+    
+}
