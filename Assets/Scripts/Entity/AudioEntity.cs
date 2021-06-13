662b44a964d6107581ea7fca629a92a67daec7bc
benk0913 <benk0913@gmail.com>
2021-06-13 14:37:19 +0300

    Coinage Prepare & Day and night audio support

--- Assets/Scripts/Entity/AudioEntity.cs
+++ Assets/Scripts/Entity/AudioEntity.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AudioEntity : MonoBehaviour
 {
+    string lastSoundscapeSet;
+
     public void PlaySound(string soundKey)
     {
         if(string.IsNullOrEmpty(soundKey))
@@ -22,5 +24,47 @@ public class AudioEntity : MonoBehaviour
         }
 
         AudioControl.Instance.PlayInPosition(soundKey, transform.position);
+    }
+
+    public void SetMusic(string musicKey)
+    {
+        AudioControl.Instance.SetMusic(musicKey);
+    }
+
+    public void SetMusicToSceneDefault()
+    {
+        AudioControl.Instance.SetMusic(CORE.Instance.ActiveSceneInfo.MusicTrack);
+    }
+
+    public void SetSoundscape(string soundscapeKey)
+    {
+        if(!string.IsNullOrEmpty(lastSoundscapeSet))
+        {
+            AudioControl.Instance.StopSound(lastSoundscapeSet);
+            lastSoundscapeSet = "";
+        }
+
+        if (!string.IsNullOrEmpty(CORE.Instance.ActiveSceneInfo.Soundscape))
+        {
+            AudioControl.Instance.StopSound(CORE.Instance.ActiveSceneInfo.Soundscape);
+        }
+
+        lastSoundscapeSet = soundscapeKey;
+        AudioControl.Instance.Play(soundscapeKey,true);
+    }
+
+    public void SetSoundscapeSceneDefault()
+    {
+        if (!string.IsNullOrEmpty(lastSoundscapeSet))
+        {
+            AudioControl.Instance.StopSound(lastSoundscapeSet);
+        }
+
+        if(string.IsNullOrEmpty(CORE.Instance.ActiveSceneInfo.Soundscape))
+        {
+            return;
+        }
+
+        AudioControl.Instance.Play(CORE.Instance.ActiveSceneInfo.Soundscape, true);
     }
 }
