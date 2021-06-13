35aba2b4e29f0050dd75fb4fbf7193300426b124
benk0913 <benk0913@gmail.com>
2021-06-13 15:11:52 +0300

    Inspect Progress

---
+++ Assets/Scripts/UI/InspectTipUI.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InspectTipUI : MonoBehaviour
+{
+    [SerializeField]
+    CanvasGroup CG;
+
+    [SerializeField]
+    TextMeshProUGUI KeyLabel;
+
+    [SerializeField]
+    TextMeshProUGUI ContentLabel;
+
+    Actor currentActor;
+
+
+
+    private void LateUpdate()
+    {
+        if(CORE.Instance == null || CORE.Instance.Room == null || CORE.Instance.Room.PlayerActor == null || CORE.Instance.Room.PlayerActor.ActorEntity == null)
+        {
+            return;
+        }
+
+        Actor playerActor = CORE.Instance.Room.PlayerActor.ActorEntity;
+
+        currentActor = CORE.Instance.Room.GetNearestActor(playerActor, true);
+
+        if (currentActor != null && Vector2.Distance(currentActor.transform.position, playerActor.transform.position) > 20)
+        {
+            currentActor = null;
+        }
+
+
+        if (currentActor == null)
+        {
+            if (CG.alpha > 0f)
+            {
+                CG.alpha -= 3f * Time.deltaTime;
+            }
+        }
+        else
+        {
+            string targetString = "to inspect " + currentActor.name; ;
+            if (ContentLabel.text != targetString)
+            {
+                KeyLabel.text = InputMap.Map["Inspect"].ToString();
+                ContentLabel.text = targetString;
+            }
+
+            if (CG.alpha < 1f)
+            {
+                CG.alpha += 3f * Time.deltaTime;
+            }
+
+            if (Input.GetKeyDown(InputMap.Map["Inspect"]))
+            {
+                InventoryUI.Instance.Show(currentActor.State.Data);
+            }
+        }
+    }
+
+
+}
