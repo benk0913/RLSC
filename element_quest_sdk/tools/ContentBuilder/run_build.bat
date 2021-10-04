cde604242145ff2623d6e099c113cb5f4daef603
benk0913 <benk0913@gmail.com>
2021-10-04 15:53:46 +0300

    script fixes

--- element_quest_sdk/tools/ContentBuilder/run_build.bat
+++ element_quest_sdk/tools/ContentBuilder/run_build.bat
@@ -1,1 +1,1 @@
-builder\steamcmd.exe  +login benk09123456 0522235730 +run_app_build M:\Unity Projects\RLSC\RLSCOnline2D\element_quest_sdk\tools\ContentBuilder\scripts\app_build_1780330.vdf
+builder\steamcmd.exe  +login benk09123456 0522235730 +run_app_build %cd%\scripts\app_build_1780330.vdf
