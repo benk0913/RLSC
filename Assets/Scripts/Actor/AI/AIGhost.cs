56819198ae1a04f3343e2502cb48a5cae82b6910
benk0913 <benk0913@gmail.com>
2021-10-11 10:54:16 +0300

    Update AIGhost.cs

--- Assets/Scripts/Actor/AI/AIGhost.cs
+++ Assets/Scripts/Actor/AI/AIGhost.cs
@@ -43,7 +43,7 @@ public class AIGhost : ActorAI
 
                     if (SelectedAbility == null)
                     {
-                        SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Ghostial Pull" && x.CurrentCD <= 0f);
+                        SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Ghost Pull" && x.CurrentCD <= 0f);
                     }
 
                 }
@@ -53,7 +53,7 @@ public class AIGhost : ActorAI
 
                     if (SelectedAbility == null)
                     {
-                        SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Ghostial Pull" && x.CurrentCD <= 0f);
+                        SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Ghost Pull" && x.CurrentCD <= 0f);
                     }
                 }
 
