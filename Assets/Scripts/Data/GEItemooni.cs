2261157c046e88763a0e25510428e0eb21565362
benk0913 <benk0913@gmail.com>
2021-10-30 14:00:39 +0300

    Misc fixes

--- Assets/Scripts/Data/GEItemooni.cs
+++ Assets/Scripts/Data/GEItemooni.cs
@@ -3,19 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "GEGetItem ", menuName = "Data/GEGetItem", order = 2)]
-public class GEGetItem : GameEvent
+[CreateAssetMenu(fileName = "GEItemooni ", menuName = "Data/GEItemooni ", order = 2)]
+public class GEItemooni : GameEvent
 {
-    public string ItemName;
-    
-
+    public ItemData targetItem;
     public override void Execute(System.Object obj = null)
     {
         base.Execute(obj);
-
         JSONNode node = new JSONClass();
-        node["item_name"] = ItemName;
+        node["item_name"] = targetItem.name;
         SocketHandler.Instance.SendEvent("ge_get_item", node);
-
     }
 }
