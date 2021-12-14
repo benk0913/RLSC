cb5f7b0f09cd0bb73e8fdf03dc41ed5e259dcd51
benk0913 <benk0913@gmail.com>
2021-12-14 14:38:03 +0200

    Mud Creatures Salamander

---
+++ Assets/Scripts/Actor/AI/AIMudCreature.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMudCreature : ActorAI
+{
+
+
+
+    protected override IEnumerator AIBehaviourRoutine()
+    {
+        yield return 0;
+        while (true)
+        {
+            AbilityState SelectedAbility = null;
+
+            while (SelectedAbility == null)
+            {
+                SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Mud Creature Chomp" && x.CurrentCD <= 0f);
+
+                if (SelectedAbility == null)
+                {
+                    SelectedAbility = Act.State.Abilities.Find(x => x.CurrentAbility.name == "Mud Creature Burp" && x.CurrentCD <= 0f);
+                }
+
+                WaitBehaviour();
+
+                yield return 0;
+            }
+            
+            yield return new WaitForSeconds(Random.Range(0f,0.2f));
+
+            yield return StartCoroutine(UseAbilityRoutine(SelectedAbility));
+            
+
+            yield return 0;
+        }
+    }
+
+
+
+
+}
