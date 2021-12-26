0d5b7b3b9540ed61977167cc1ef3ce0c2be185bf
benk0913 <benk0913@gmail.com>
2021-12-27 01:41:02 +0200

    fixes

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -328,7 +328,7 @@ public class MainMenuUI : MonoBehaviour
 
        public void ShowStorePage()
     {
-        Application.OpenURL("https://discord.gg/nnB9V4hr");
+        Application.OpenURL("https://discord.gg/NbZgMpRxqr");
         //Steamworks.SteamFriends.ActivateGameOverlayToStore(new Steamworks.AppId_t(1780330), Steamworks.EOverlayToStoreFlag.k_EOverlayToStoreFlag_None);
     }
 
